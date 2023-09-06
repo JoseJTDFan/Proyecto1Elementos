5083364baee0f350ec17d6557a6a03a854bde751
José Julián Brenes Garro <josejbrenes03@gmail.com>
2023-09-06 15:41:01 -0600

    mas cosasd

--- interfaz.py
+++ interfaz.py
@@ -17,15 +17,24 @@ import math
 
 mainWindow=Tk()
 mainWindow.config(bg="#4da8cf")
-mainWindow.geometry("800x350")
-mainWindow.title("Personalidades")
+mainWindow.geometry("1000x350")
+mainWindow.title("Crear tríangulos")
 mainWindow.resizable(False,False)
+mainWindow.iconbitmap('triangulo.ico')
+
+canvas = turtle.ScrolledCanvas(mainWindow)
+canvas.pack(side=tk.RIGHT, fill=tk.BOTH, expand=tk.YES)
+
+t = turtle.RawTurtle(canvas)
+t.pensize(2)
+t.shape("turtle")
+t.color("darkgreen", "green")
+#turtle.title("Realizar triángulo")
+#t.bgcolor("lightblue")
 
 def hola():
+    
     '''
-    t = turtle.Turtle()
-    turtle.bgcolor("lightblue")
-    turtle.title("Realizar triángulo")
     t.fd(45)
     t.lt(120)
     t.fd(30)
@@ -34,24 +43,25 @@ def hola():
     t.lt(120)
     t.fd(22.5)
     t.circle(28.86751346)'''
-    a=200
-    b=300
-    c=400
+    a=90
+    b=150
+    c=120
     if a + b > c and a + c > b and b + c > a:
         # Calcular los ángulos usando la ley de los cosenos
-        #A = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+        A = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
         B = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
         C = math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
-        
-        print(B)
-        print(C)
+
 
         # Dibujar el triángulo
-        t = turtle.Turtle()
+        t.pencolor("blue")
+        verticeUno = t.pos()
         t.forward(c)
         t.left(180 - B)
+        verticeDos = t.pos()
         t.forward(a)
         t.left(180 - C)
+        verticeTres = t.pos()
         t.forward(b)
 
         # Calcular el semiperímetro
@@ -59,14 +69,41 @@ def hola():
 
         # Calcular el área del triángulo usando la fórmula de Herón
         area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+        
+        Ix = (a * verticeUno[0] + b * verticeDos[0] + c * verticeTres[0]) / (a + b + c)
+        Iy = (a * verticeUno[1] + b * verticeDos[1] + c * verticeTres[1]) / (a + b + c)
 
         # Calcular el radio del círculo inscrito
         r = area / s
-
         t.penup()
-        t.goto(-r, 0)  # Mover el cursor al centro del círculo inscrito
+        t.goto(Ix, Iy)
         t.pendown()
+        t.dot(5,"purple")
+        t.penup()
+        t.goto(Ix, Iy-r)
+        t.left(180-A)
+        t.pendown()
+        #t.penup()
+        #turtle.setheading(270)
+        #turtle.setheading(0)
+        #t.pendown()
+        t.pencolor("red")
         t.circle(r)
+        t.pencolor("purple")
+        t.penup()
+        t.goto(Ix, Iy)
+        t.pendown()
+        t.goto(verticeUno)
+        t.penup()
+        t.goto(Ix, Iy)
+        t.pendown()
+        t.goto(verticeDos)
+        t.penup()
+        t.goto(Ix, Iy)
+        t.pendown()
+        t.goto(verticeTres)
+        
+
 
 #Título
 frameTitulo=Frame(mainWindow,width=300,height=100)
@@ -78,14 +115,14 @@ frame=Frame(mainWindow,width=3000,height=2000)
 frame.config(bg="#4da8cf")
 frame.pack()
 
-titulo=Label(frameTitulo,text="No soy un círculo vicioso, soy un tríangulo amoroso")
+titulo=Label(frameTitulo,text="No soy un círculo vicioso,\n soy un tríangulo amoroso")
 titulo.grid(pady=10)
 titulo.config(bg="#4da8cf",font=("arial",17,"bold"))
 
-cargarBD=Button(frame,text="Registrar datos de una persona",command=hola)
+cargarBD=Button(frame,text="Crear triángulo",command=hola)
 cargarBD.grid(row=3,column=1,pady=5)
 cargarBD.config(bg="#66bde6", width=44)
-
+'''
 registroDinamico=Button(frame,text="Registro dinámico")
 registroDinamico.grid(row=4,column=1,pady=5)
 registroDinamico.config(bg="#66bde6",width=44)
@@ -104,7 +141,7 @@ xml.config(bg="#66bde6",width=44)
 
 reporteBoton=Button(frame,text="Reportes")
 reporteBoton.grid(row=8,column=1,pady=5)
-reporteBoton.config(bg="#66bde6",width=44)
+reporteBoton.config(bg="#66bde6",width=44)'''
 
 salir=Button(frame,text="Salir",command=mainWindow.quit)
 salir.grid(row=9,column=1,pady=5)
